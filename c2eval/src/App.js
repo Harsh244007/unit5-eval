c694c73c419a201bbd435ad59044822b2bc75ded
Harsh Patel <97446580+Harsh244007@users.noreply.github.com>
2022-04-18 16:46:24 +0530

    main

--- c2eval/src/App.js
+++ c2eval/src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn fun
+          Learn jss
+          
         </a>
       </header>
     </div>
