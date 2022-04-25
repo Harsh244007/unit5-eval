f398a778fa4a7b3e20acc7cf71234f60fbce86bb
Harsh Patel <97446580+Harsh244007@users.noreply.github.com>
2022-04-25 16:46:34 +0530

    main

--- c3eval/src/components/Navbar.jsx
+++ c3eval/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import {Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-      <div className="navbar" style={{ display: "flex", padding:"20px",margin:"20px 20px 20px 20px",height: "30px", justifyContent: "center" }}>
+      <div className="navbar" style={{ display: "flex", padding:"20px",margin:"20px 20px 20px 20px",height: "30px", justifyContent: "center",gap:"15px" }}>
 
       <Link className="nav-home" to="/">
         Home
